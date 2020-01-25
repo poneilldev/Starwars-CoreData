0039edefa353b06af2ddb50427ac6b652e270c81
Paul O'Neill <paul@neiybor.com>
2020-01-25 10:22:21 -0700

    initial setup

---
+++ StarWarz/StarWarz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarWarz
+//
+//  Created by Paul O'Neill on 1/25/20.
+//  Copyright © 2020 Paul O'Neill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
