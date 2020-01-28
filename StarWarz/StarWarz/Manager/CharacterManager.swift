222016013980d3b768687dded34fefad99a54821
Paul O'Neill <paul@neiybor.com>
2020-01-28 08:56:43 -0700

    Be sure to clear cache every time you make this service call

--- StarWarz/StarWarz/Manager/CharacterManager.swift
+++ StarWarz/StarWarz/Manager/CharacterManager.swift
@@ -14,7 +14,28 @@ struct CharacterManager {
     let cdManager = CoreDataManager()
 
     func getCharacterList(completion: @escaping (Result<StarWarzCharacters, Error>) -> Void) {
+        // clear cache
+        clearCharacters()
+
+        // ask server for updated list
         service.getCharacterList(completion: completion)
         cdManager.saveContext()        
+    }
+
+    func clearCharacters() {
+        let context = cdManager.persistentContainer.viewContext
+        let request: NSFetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "SWCharacter")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+        do {
+            deleteRequest.resultType = .resultTypeObjectIDs
+            let result = try context.execute(deleteRequest) as? NSBatchDeleteResult
+            if let objectIDArray = result?.result as? [NSManagedObjectID] {
+                let changes = [NSDeletedObjectsKey: objectIDArray]
+                NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [context])
+            }
+            try context.save()
+        } catch let error {
+            print(error)
+        }
     }
 }
